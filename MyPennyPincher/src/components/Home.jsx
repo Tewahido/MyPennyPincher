074549be48f1b2685768cded06595977e45477c3
Tewahido <tewahidotefera42@gmail.com>
2025-05-06 11:39:16 +0200

    Navbar completed

---
+++ MyPennyPincher/src/components/Home.jsx
@@ -0,0 +1,5 @@
+import MoneyImage from "../assets/background_image.jpeg";
+
+export default function Home() {
+  return <></>;
+}
---
+++ MyPennyPincher/src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom";
+import Logo from "../assets/GrabbingMoneyColor_Icon.png";
+
+export default function Navbar() {
+  return (
+    <div className="flex flex-col fixed top-0 left-0 w-full z-10">
+      <nav className="flex justify-between h-20 mx-10 ">
+        <div className="flex items-end h-full ">
+          <img
+            src={Logo}
+            alt="Logo"
+            className="mb-3 md:mb-2 lg:mb-0 h-8 md:h-10 lg:h-15 cursor-pointer"
+          />
+          <Link
+            to="/"
+            className="text-green-700 font-bold text-xl md:text-2xl lg:text-3xl mb-3 hover:text-green-600 transition duration-150"
+          >
+            MyPennyPincher
+          </Link>
+        </div>
+        <div className=" flex items-end h-full gap-10">
+          <Link
+            to="/dashboard"
+            className="text-green-700 font-bold sm:text-lg lg:text-xl mb-3 hover:underline hover:text-green-600 transition duration-300"
+          >
+            Dashboard
+          </Link>
+          <Link
+            to="/logout"
+            className="text-green-700 font-bold sm:text-lg lg:text-xl mb-3 hover:underline hover:text-green-600 transition duration-300"
+          >
+            Logout
+          </Link>
+        </div>
+      </nav>
+      <div className=" mx-auto h-0.5 w-[90vw] bg-green-700"></div>
+    </div>
+  );
+}
