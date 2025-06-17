0a529257805b72a4f37f0164f739e925ee29b719
Tewahido Tefera <tewahidotefera42@gmail.com>
2025-06-17 13:11:50 +0200

    Changed file structure for models in api

---
+++ MyPennyPincher_API/MyPennyPincher_API/Models/Config Models/JwtOptions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class JwtOptions
+{
+    public const string JwtSection = "Jwt";
+
+    public string Key { get; set; }
+    public string Issuer { get; set; }
+    public int TokenValidityMins { get; set; }
+}
