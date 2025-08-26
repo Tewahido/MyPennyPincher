bf361191b5e490839c597d20aaaa35a978d23638
Tewahido Tefera <tewahidotefera42@gmail.com>
2025-08-26 13:54:51 +0200

    Replaced conrete dependencies from auth service tests with substitutions

--- MyPennyPincher_API/MyPennyPincher_API/Services/AuthService.cs
+++ MyPennyPincher_API/MyPennyPincher_API/Services/AuthService.cs
@@ -3,7 +3,6 @@ using MyPennyPincher_API.Models.DataModels;
 using MyPennyPincher_API.Models.DTO;
 using MyPennyPincher_API.Repositories.Interfaces;
 using MyPennyPincher_API.Services.Interfaces;
-using System.Text.RegularExpressions;
 
 namespace MyPennyPincher_API.Services;
 
--- MyPennyPincher_API/MyPennyPincher_API_Tests/Unit Tests/AuthServiceTest.cs
+++ MyPennyPincher_API/MyPennyPincher_API_Tests/Unit Tests/AuthServiceTest.cs
@@ -1,24 +1,21 @@
-﻿using MyPennyPincher_API.Context;
-using MyPennyPincher_API.Exceptions;
+﻿using MyPennyPincher_API.Exceptions;
 using MyPennyPincher_API.Models.DataModels;
 using MyPennyPincher_API.Models.DTO;
-using MyPennyPincher_API.Repositories;
 using MyPennyPincher_API.Repositories.Interfaces;
 using MyPennyPincher_API.Services;
 using MyPennyPincher_API_Tests.Test_Utilities;
+using NSubstitute;
 
 namespace MyPennyPincher_API_Tests.Unit_Tests;
 
-public class AuthServiceTest : IDisposable
+public class AuthServiceTest
 {
     private readonly AuthService _authService;
-    private readonly MyPennyPincherDbContext _context;
     private readonly IAuthRepository _authRepository; 
 
     public AuthServiceTest() 
     {
-        _context = DbContextFactory.GenerateInMemoryDB();
-        _authRepository = new AuthRepository(_context);
+        _authRepository = Substitute.For<IAuthRepository>();
         _authService = new AuthService(_authRepository);
     }
 
@@ -26,19 +23,32 @@ public class AuthServiceTest : IDisposable
     public async Task GIVEN_User_WHEN_Registering_THEN_ReturnNewUser()
     {
         //Arrange
-        var   user = TestDataFactory.CreateTestUser();
+        var user = TestDataFactory.CreateTestUser();
+
+        _authRepository.FindByEmailAsync(user.Email)
+            .Returns(Task.FromResult<User?>(null));
 
         //Act
         var registeredUser = await _authService.Register(user);
 
         var passwordIsHashed = BCrypt.Net.BCrypt.Verify(user.Password, registeredUser.Password);
 
-        var expectedUser = _context.Users.FirstOrDefault(u => u.UserId == user.UserId);
-
         //Assert
         Assert.NotNull(registeredUser);
         Assert.True(passwordIsHashed);
-        Assert.NotNull(expectedUser);
+    }
+
+    [Fact]
+    public async Task GIVEN_ExistingUser_WHEN_Registering_THEN_ReturnUserAlreadyExistsException()
+    {
+        //Arrange
+        var user = TestDataFactory.CreateTestUser();
+
+        _authRepository.FindByEmailAsync(user.Email)
+            .Returns(user);
+
+        //Act & Assert
+        await Assert.ThrowsAsync<UserAlreadyExistsException>(() => _authService.Register(user));
     }
 
     [Fact]
@@ -73,9 +83,15 @@ public class AuthServiceTest : IDisposable
         //Arrange
         var user = TestDataFactory.CreateTestUser();
 
+        _authRepository.FindByEmailAsync(user.Email)
+           .Returns(Task.FromResult<User?>(null));
+
         var registeredUser = await _authService.Register(user);
 
         var login = TestDataFactory.CreateUserLogin(user);
+
+        _authRepository.FindByEmailAsync(login.Email)
+            .Returns(registeredUser);
 
         //Act
         var expectedUser = await _authService.Login(login);
@@ -101,10 +117,5 @@ public class AuthServiceTest : IDisposable
 
         //Act & Assert
         await Assert.ThrowsAsync<InvalidCredentialsException>(async () => await _authService.Login(login));
-    }
-
-    public void Dispose()
-    {
-        _context?.Dispose();
     }
 }
